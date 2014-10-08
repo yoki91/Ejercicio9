a6acd28910caea728ae6ffa397cbbaa27daec4fa
yifei ge <yokihappy@hotmail.com>
2014-10-08 17:45:37 +0200

    Initial commit

---
+++ src/main/java/edu/upc/eetac/dsa/yifeige/Ejercicio9/CuentaAtras.java
@@ -0,0 +1,41 @@
+package edu.upc.eetac.dsa.yifeige.Ejercicio9;
+
+public class CuentaAtras implements Runnable 
+{
+	Thread th;
+	String id;
+	int numero;
+	int segundo=0;
+	public CuentaAtras(String s,int n)
+	{
+		this.id=s;
+		this.numero=n;
+		th=new Thread(this);
+		th.start();
+		
+	}
+	public void run()
+	{
+		while (numero >= 0) 
+		{
+			System.out.printf("El %s su numero actual es %d\n", id, numero);
+			numero = numero - 1;
+			try 
+			{
+
+				th.sleep(1*1000);
+				segundo = segundo + 1;
+				System.out.printf("Para el thread %s Ha pasado %d segundos\n",id, segundo);
+
+			} 
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+			}
+		   }
+	}
+	
+}
+	
+
+	
---
+++ src/main/java/edu/upc/eetac/dsa/yifeige/Ejercicio9/PruebaThreads1.java
@@ -0,0 +1,17 @@
+package edu.upc.eetac.dsa.yifeige.Ejercicio9;
+import edu.upc.eetac.dsa.yifeige.Ejercicio9.CuentaAtras;
+
+
+public class PruebaThreads1 
+{
+    public static void main( String[] args )
+    {
+    	CuentaAtras contador1 = new CuentaAtras("ID1", 4);
+		CuentaAtras contador2 = new CuentaAtras("ID2", 7);
+		CuentaAtras contador3 = new CuentaAtras("ID3", 8);
+    	
+    	
+    	
+        
+    }
+}
